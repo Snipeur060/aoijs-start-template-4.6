b876b777105603a45d07269950ab6aa7b7ca41e7
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2022-01-17 17:22:50 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const aoijs = require("aoi.js")
+ 
+const bot = new aoijs.Bot({
+  token: "token", 
+  prefix: "prefix" 
+})
+bot.onMessage() // Allows Commands to Executed
+bot.loadCommands(`./command/`)
+bot.variables({
+blacklist: "false",
+name: ""
+  })
+bot.status({
+    text: "Préfix : thepref",
+    type: "PLAYING",
+    status: "dnd",
+    time: 12
+  })
